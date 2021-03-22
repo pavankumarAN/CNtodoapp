e2337ed0cd41070ad117fc2210e29f9b0f380227
pavankumarAN <pavankumaran51@gmail.com>
2021-03-22 20:12:23 +0530

    added authentication for comments section and make it available in nav bar

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/post.controller');
+const passport = require('passport');
 
 
 router.get('/comment',postController.comment);
 
-router.post('/createpost',postController.createpost);
+router.post('/createpost',passport.checkAuthentication,postController.createpost);
 
 module.exports = router;
