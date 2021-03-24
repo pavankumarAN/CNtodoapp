425513ef4b54f7d34c674dc87ba72a5f92c0c7ec
pavankumarAN <pavankumaran51@gmail.com>
2021-03-24 19:46:33 +0530

    Initial set-up for image upload

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const imagePath = path.join('/uploads/user/images');
 
 const UserSchema = new mongoose.Schema({
     email: {
